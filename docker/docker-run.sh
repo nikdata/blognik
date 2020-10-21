3de6c259e2d731a3174b1b344d549b68e0837102
Nikhil Agarwal <git@niks.me>
2020-10-20 19:47:21 -0500

    updates to docker, shell, and new post

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,1 +1,1 @@
-docker run -d -e PASSWORD=1234 -v ~/Documents/:/home/rstudio/projects/ -p 3838:3838 -p 8787:8787 -p 80:80 -p 4321:4321 randomnik_blog:latest
+docker run -d -e PASSWORD=1234 -v ~/Documents/:/home/rstudio/projects/ -p 3838:3838 -p 8787:8787 -p 80:80 -p 4321:4321 -p 4870:4870 randomnik_blog:latest
